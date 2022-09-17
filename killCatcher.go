0bb0369f3a7705bd5e1bbde93bd12f96906ef840
Miroslav Repka <42786030+MiroslavRepka@users.noreply.github.com>
2022-09-17 14:53:42 +0200

    Add comments (#3)

--- killCatcher.go
+++ killCatcher.go
@@ -9,34 +9,44 @@ import (
 )
 
 const (
-	sleepTime = 5 * time.Second
+	sleepTime = 5 * time.Second // interval in which the goroutine will check for SIGTERM
 )
 
-type KillCatcher struct {
-	postKillFunc func() error
+// killCatcher will execute provided function postKillFunc after the SIGTERM is received
+// if the function returns an error, it will be propagated to the main program for handling
+type killCatcher struct {
+	postKillFunc func() error //function which will be executed after SIGTERM
 }
 
-func New(f func() error) *KillCatcher {
-	return &KillCatcher{postKillFunc: f}
+// New returns new killCatcher with provided function which will be executed after SIGTERM
+func New(f func() error) *killCatcher {
+	return &killCatcher{postKillFunc: f}
 }
 
-func (k *KillCatcher) Listen() error {
+// Listen listens for SIGTERM and executes provided function in killCatcher if received
+func (k *killCatcher) Listen() error {
+	//create channel to listen for SIGTERM
 	term := make(chan os.Signal, 1)
 	signal.Notify(term, syscall.SIGTERM)
 	defer func() {
 		signal.Stop(term)
 	}()
+	// Listen for SIGTERM
 	for {
 		select {
+		// the SIGTERM received
 		case <-term:
 			fmt.Printf("== Received SIGTERM signal, executing provided function ==\n")
+			// execute postKillFunc function
 			if err := k.postKillFunc(); err != nil {
 				fmt.Printf("== Error while executing function post SIGTERM : %v\n", err)
 				return fmt.Errorf("error while executing function post SIGTERM : %v", err)
 			}
 			return nil
+		// default is used so the execution is not blocked
 		default:
 			fmt.Printf("== DEBUG TICK ==\n")
+			// sleep for sleepTime
 			time.Sleep(sleepTime)
 		}
 	}
